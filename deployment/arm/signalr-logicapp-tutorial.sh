f53d11303033d9a1dd59cb9fe2e16962addfe9d9
micgreen@microsoft.com <micgreen@microsoft.com>
2020-04-14 17:25:07 -0500

    Update json and shell.

---
+++ deployment/arm/signalr-logicapp-tutorial.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -eu
+
+# change directory to current directory
+parent_path=$(
+    cd "$(dirname "${BASH_SOURCE[0]}")"
+    pwd -P
+)
+cd "$parent_path"
+
+# set parameters
+while getopts "o:" opt; do
+    case $opt in
+        o)
+            orgName=$OPTARG
+        ;;
+        \?)            
+            echo "Invalid parameters! Required parameters are:  [-o] orgName"
+        ;;   
+    esac
+done
+
+# ensure parameters were provided
+if [[ $# -eq 0 || -z $orgName ]]; then
+    echo "Parameters missing! Required parameters are:  [-n] orgName"
+    exit 1; 
+fi
+
+# set local variables
+declare rgName="rg-$orgName-gha-scus-dev"
+declare signalrName="sr-$orgName-gha-scus-dev"
+declare logicAppName="la-$orgName-gha-scus-dev"
+
+# create the azure resource group
+az group create --l southcentralus \
+    -n $rgName \
+    --tags "githubactions"
+
+# create the azure deployment
+az deployment group create -g $rgName \
+    -n "infra-$orgName-deployment" \
+    -p name=$signalrName \
+    -f signalr.json \
+    --output none
+
+# create the azure deployment
+az deployment group create -g $rgName \
+    -n "la-$orgName-deployment" \
+    -f logicapp.json \
+    --output none    
+
+# get primary connection string
+declare signalrPrimaryConnectionString=$(az signalr key list -n $signalrName -g $rgName --query primaryConnectionString -o tsv)
+
+# TODO: check for null/empty
+#       put keyvault name in secrets and pass as parameter
+az keyvault secret set -n "kv-secret-$signalrName-primary-connection-string" \
+    --vault-name kv-gha-scus-dev \
+    --value $signalrPrimaryConnectionString \
+    --output none
